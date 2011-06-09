79abb2147b4befe8bd9acccfc3c9c4207e3396e1
Andrea Paolini <andrea.paolini@tasgroup.it>
2011-06-09 17:55:29 +0200

    Further fixes for WMQv6

--- lib/jms/mq_workaround.rb
+++ lib/jms/mq_workaround.rb
@@ -2,35 +2,67 @@
 
 begin
   
-  class com.ibm.mq.jms::MQQueueSession    
-    def consume(params, &proc)
-      Java::JavaxJms::Session.instance_method(:consume).bind(self).call(params, &proc)
+  class com.ibm.mq.jms::MQQueueSession
+    
+    if self.instance_methods.include? "consume"
+      def consume(params, &proc)
+        Java::JavaxJms::Session.instance_method(:consume).bind(self).call(params, &proc)
+      end
     end
+
   end
 
-  class com.ibm.mq.jms::MQSession    
-    def consume(params, &proc)
-      Java::JavaxJms::Session.instance_method(:consume).bind(self).call(params, &proc)
+
+  class com.ibm.mq.jms::MQSession
+
+    if self.instance_methods.include? "consume"
+      def consume(params, &proc)
+        Java::JavaxJms::Session.instance_method(:consume).bind(self).call(params, &proc)
+      end
     end
+    
+    if self.instance_methods.include? "create_destination"
+      def create_destination(params)
+        Java::JavaxJms::Session.instance_method(:create_destination).bind(self).call(params)
+      end
+    end
+
   end
+
   
   class com.ibm.mq.jms::MQQueueBrowser
-    def each(params, &proc)
-      Java::ComIbmMsgClientJms::JmsQueueBrowser.instance_method(:each).bind(self).call(params, &proc)
-    end
-  end
 
-  class com.ibm.mq.jms::MQQueueReceiver
-    def each(params, &proc)
-      Java::JavaxJms::MessageConsumer.instance_method(:each).bind(self).call(params, &proc)
-    end
-    def get(params={})
-      Java::JavaxJms::MessageConsumer.instance_method(:get).bind(self).call(params)
+    if self.instance_methods.include? "each"
+      def each(params, &proc)
+        Java::ComIbmMsgClientJms::JmsQueueBrowser.instance_method(:each).bind(self).call(params, &proc)
+      end
     end
   end
   
+
+  class com.ibm.mq.jms::MQQueueReceiver
+    
+    if self.instance_methods.include? "each"
+      def each(params, &proc)
+        Java::JavaxJms::MessageConsumer.instance_method(:each).bind(self).call(params, &proc)
+      end
+    end
+    
+    if self.instance_methods.include? "get"
+      def get(params={})
+        Java::JavaxJms::MessageConsumer.instance_method(:get).bind(self).call(params)
+      end
+    end
+    
+  end
+  
+  
   class com.ibm.mq.jms::MQQueue
+    
+    if self.instance_methods.include? "delete"
     undef_method :delete
+    end
+    
   end
 
 rescue NameError
